e2bdf275408d27ed87f9095108f494acf019cd81
Ginette Lafit <ginettelafit19@gmail.com>
2020-04-30 21:18:13 +0200

    Update ui.R

--- R/ui.R
+++ R/ui.R
@@ -23,7 +23,7 @@ ui <- shinyUI(pageWithSidebar(
 
   # Sidebar with controls to select the outputs to compute power
   sidebarPanel(
-      useShinyjs(),
+      shinyjs::useShinyjs(),
       id = "side-panel",
       withMathJax(),
 
