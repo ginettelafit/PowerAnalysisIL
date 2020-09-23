0c850f438b7b3caf77bc282c434a86f2ad0b96b4
Ginette Lafit <ginettelafit19@gmail.com>
2020-09-23 09:51:48 +0200

    Update time.sim.R

--- R/time.sim.R
+++ R/time.sim.R
@@ -13,10 +13,10 @@ sigma, rho, sigma.v0, sigma.v1,rho.v,
 mu.W, sigma.W, mu.X, mu.X0, mu.X1, sigma.X, 
 sigma.X0, sigma.X1,
 is.rho.zero,isW.center,isX.center,Ylag.center, 
-alpha,50,Opt.Method)
+alpha,10,Opt.Method)
 end = Sys.time() 
 
-time.hat = round(as.numeric(end-start)*R/(50*3600),3)
+time.hat = round(as.numeric(end-start)*R/(10*3600),3)
 time.sim = paste('The estimated computational time is',time.hat,'hours')
 
 return(list(time.sim=time.sim,time.hat=time.hat))
