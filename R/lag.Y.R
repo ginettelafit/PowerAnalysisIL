8ec6612709024d17fbe5b6b7712e196834ee6fff
Ginette Lafit <ginettelafit19@gmail.com>
2023-06-07 12:59:50 +0200

    Update lag.Y.R

--- R/lag.Y.R
+++ R/lag.Y.R
@@ -11,7 +11,7 @@ Y_lag = rep(0,nrow(data))
 subjno.i = unique(data$subjno)
 for (i in subjno.i){
 n.i = which(data$subjno==i)
-Y_lag[n.i] = shift(data$Y[n.i],1)
+Y_lag[n.i] = data.table::shift(data$Y[n.i],1)
 }
 
 return(Y_lag)
